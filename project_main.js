19a47a0068a3316759690d2d2248202f16e572c7
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2025-05-26 12:25:33 +0530

    updt

--- project_main.js
+++ project_main.js
@@ -1,6 +1,6 @@
 // 1. Deposit some amount.
-//2. Determine total lines of bet
-//3. Collect the bet amount.
+// 2. Determine total lines of bet
+// 3. Collect the bet amount.
 // 4. Spin the slot machine.
 // 5. check if user won
 // 6. return the winnings / deduct the losing bet.
