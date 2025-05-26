514138ab469f86f632dfcdebb080f6e5a67fd81e
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2025-05-26 13:02:11 +0530

    updt

--- project_main.js
+++ project_main.js
@@ -5,3 +5,21 @@
 // 5. check if user won
 // 6. return the winnings / deduct the losing bet.
 // 7. Play Again.
+
+const prompt = require("prompt-sync")();
+
+const Deposit = ()=> {
+
+    while(true) {
+        const depositAmount_str = prompt("Enter amount to deposit: ");
+    let depositAmount = parseFloat(depositAmount_str);
+
+    if(isNaN(depositAmount) || depositAmount < 5) {
+        console.log("Enter a amount above 5. Try Again!");
+    }else {
+        return depositAmount;
+    }
+    }
+}
+
+const depositeAmt = Deposit();   // Holding the value returned from Deposit() func.
